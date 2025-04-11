99cd8a7ffdbcd025046f5822b1d0f0a023fda2b8
tttffff <tristanfellows@icloud.com>
2025-04-11 22:36:09 +0100

    Allow for using custom paper trail version classes

---
+++ spec/support/product.rb
@@ -0,0 +1,17 @@
+require 'paper_trail'
+require 'paper_trail-globalid'
+
+# See https://github.com/paper-trail-gem/paper_trail#6a-custom-version-classes
+
+class ApplicationVersion < ActiveRecord::Base
+  include PaperTrail::VersionConcern
+  self.abstract_class = true
+end
+
+class ProductVersion < ApplicationVersion
+  self.table_name = :product_versions
+end
+
+class Product < ActiveRecord::Base
+  has_paper_trail versions: { class_name: "ProductVersion" }
+end
